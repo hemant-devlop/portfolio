c32dedd5a1b2aced6c95f5f82454e60a0ff975d2
Hemant Kumar <8114hd@gmail.com>
2024-02-09 22:58:08 +0530

    update resume and skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,50 +1,61 @@
 import React from 'react'
 import './skills.css'
 const Skills = () => {
- 
-  return (
-    <div className='container skills'>
-      <h1 className='font text-center py-3 text-decoration-underline'>My Skills With Letest Trends</h1>
-      <div className='d-flex flex-wrap gap-5 justify-content-center p-3'>
-        <div className='skill-box'>
-            <img src="../assets/html5.png" alt="" />
+
+    return (
+        <div className='container skills'>
+            <h1 className='font text-center py-3 text-decoration-underline'>My Skills With Letest Trends</h1>
+            <div className='d-flex flex-wrap gap-5 justify-content-center p-3'>
+                <div className='skill-box' >
+                    <img src="../assets/html5.png" alt="" data-bs-toggle='tooltip' data-bs-title="hello" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/css.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/js.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/bootstrap.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/react.png" alt="" />
+                </div>
+                <div className='skill-box' >
+                    <img src="../assets/node.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    {/* <span className='fs-4'>Express JS</span> */}
+                    <img src="../assets/express.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/mongodb.png" alt="" />
+                </div>
+                <div className='skill-box' >
+                    <img src="../assets/MUI.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/github.png" alt="" style={{ height: '80px', width: '80px' }} />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/api.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/redux.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/react router v6.0.jpg" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <img src="../assets/Git_icon.svg.png" alt="" />
+                </div>
+                <div className='skill-box'>
+                    <button className='btn rounded-0'>Coming...</button>
+                </div>
+                
+            </div>
         </div>
-        <div className='skill-box'>
-            <img src="../assets/css.png" alt=""/>
-        </div>
-        <div className='skill-box'>
-            <img src="../assets/js.png" alt=""/>
-        </div>
-        <div className='skill-box'>
-            <img src="../assets/bootstrap.png" alt=""/>
-        </div>
-        <div className='skill-box'>
-            <img src="../assets/react.png" alt=""/>
-        </div>
-        <div className='skill-box' >
-            <img src="../assets/node.png" alt=""/>
-        </div>
-        <div className='skill-box'>
-            <span className='fs-4'>Express</span><img src="../assets/express.png" alt="" style={{height:'80px'}}/>
-        </div>
-        <div className='skill-box'>
-            <img src="../assets/mongodb.png" alt=""/>
-        </div>
-        <div className='skill-box' >
-            <img src="../assets/MUI.png" alt=""/>
-        </div>
-        <div className='skill-box'>
-            <img src="../assets/github.png" alt=""  style={{height:'80px',width:'80px'}}/>
-        </div>
-        <div className='skill-box'>
-            <img src="../assets/api.png" alt="" />
-        </div>
-        <div className='skill-box'>
-            <button className='btn rounded-0'>Coming...</button>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Skills
