d10398cb7b67fc08ebfb8003c037a47e35876409
Hemantkumar <8114hd@gmail.com>
2025-12-10 22:33:34 +0530

    update001

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
   server: {
     host: true,       // or use '0.0.0.0'
     port: 5173,       // default port
